8b2b3000fc7a68f60fc44cc221e711d9b6ed23ae
flaticols <gh@flaticols.dev>
2024-02-25 23:05:28 +0100

    Simplified, added tests

--- muxx.go
+++ muxx.go
@@ -1,46 +1,77 @@
 package muxx
 
 import (
-	"errors"
 	"net/http"
+	"regexp"
 )
 
-type Option func(*Options) error
+type Middleware func(http.Handler) http.Handler
 
-type Options struct {
-	Mux *http.ServeMux
+type Group struct {
+	groupPath   string
+	middlewares []Middleware
+	mux         *http.ServeMux
 }
 
-func WithMux(mux *http.ServeMux) Option {
-	return func(o *Options) error {
-		o.Mux = mux
-		return nil
+// New creates a new routes group with a specified base path on top of the existing bundle.
+func New() (*Group, error) {
+	return &Group{
+		mux: http.NewServeMux(),
+	}, nil
+}
+
+// Mount creates a new routes group with a specified base path on top of the existing bundle.
+func Mount(mux *http.ServeMux, basePath string) *Group {
+	return &Group{
+		mux:       mux,
+		groupPath: basePath,
 	}
 }
 
-func (o *Options) Apply(opts ...Option) error {
-	var err error
-	for _, opt := range opts {
-		err := opt(o)
-		if err != nil {
-			err = errors.Join(err, err)
+// ServeHTTP implements the http.Handler interface
+func (b *Group) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	b.mux.ServeHTTP(w, r)
+}
+
+// Mount creates a new routes group with a specified base path on top of the existing bundle.
+func (b *Group) Mount(groupPath string) *Group {
+	middlewares := make([]Middleware, len(b.middlewares))
+	copy(middlewares, b.middlewares)
+	return &Group{
+		mux:         b.mux,
+		groupPath:   b.groupPath + groupPath,
+		middlewares: middlewares,
+	}
+}
+
+// Use adds a middleware(s) to the group.
+func (b *Group) Use(middlewares ...Middleware) {
+	b.middlewares = append(b.middlewares, middlewares...)
+}
+
+var httpVerb = regexp.MustCompile(`^(\S*)\s+(.*)$`)
+
+// Handle registers a new handler with the given path and method.
+func (b *Group) Handle(path string, handler http.HandlerFunc) {
+	w := func(h http.Handler, mws ...Middleware) http.Handler {
+		if len(mws) == 0 {
+			return h
+		}
+		res := h
+		for i := len(mws) - 1; i >= 0; i-- {
+			res = mws[i](res)
+		}
+		return res
+	}
+
+	if b.groupPath != "" {
+		matches := httpVerb.FindStringSubmatch(path)
+		if len(matches) > 2 {
+			path = matches[1] + " " + b.groupPath + matches[2]
+		} else {
+			path = b.groupPath + path
 		}
 	}
 
-	return err
-}
-
-func New(rootPath string, options ...Option) (*Group, error) {
-	o := &Options{
-		Mux: http.NewServeMux(),
-	}
-	err := o.Apply(options...)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Group{
-		mux:       o.Mux,
-		groupPath: rootPath,
-	}, nil
+	b.mux.HandleFunc(path, w(handler, b.middlewares...).ServeHTTP)
 }
---
+++ muxx_test.go
@@ -0,0 +1,81 @@
+package muxx_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/flaticols/muxx"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func testMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("X-Test", "true")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func TestGroup_Mount(t *testing.T) {
+
+}
+
+func TestGroup_Middleware(t *testing.T) {
+	mux := http.NewServeMux()
+	g := muxx.Mount(mux, "/test")
+	g.Use(testMiddleware)
+
+	g.Handle("/test", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	// make a request to the test handler
+	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodGet, "/test/test", http.NoBody)
+	mux.ServeHTTP(recorder, request)
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+	assert.Equal(t, "true", recorder.Header().Get("X-Test"))
+}
+
+func TestGroup_Handle(t *testing.T) {
+	mux, err := muxx.New()
+	require.NoError(t, err)
+
+	// add a test handler
+	mux.Handle("/test", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	// make a request to the test handler
+	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodGet, "/test", http.NoBody)
+	mux.ServeHTTP(recorder, request)
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+}
+
+func TestMount(t *testing.T) {
+	mux := http.NewServeMux()
+	basePath := "/mounted"
+	g := muxx.Mount(mux, basePath)
+
+	g.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("X-Mounted-Test-Middleware", "true")
+			next.ServeHTTP(w, r)
+		})
+	})
+
+	g.Handle("/test", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodGet, basePath+"/test", http.NoBody)
+	mux.ServeHTTP(recorder, request)
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+	assert.Equal(t, "true", recorder.Header().Get("X-Mounted-Test-Middleware"))
+}
